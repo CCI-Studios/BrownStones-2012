1528bc7eaf579c9eb2a5ddc616f650c7fdaeb2e9
Matt Elliott <melliott@ccistudios.com>
2012-09-27 12:47:24 -0400

    default template files

---
+++ template/templates/brownstones/component.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<jdoc:include type="head" />
+
+	<script>
+	if (window.location.href.indexOf('print=1') != -1) {
+		window.print();
+	}
+	</script>
+</head>
+
+<body>
+	<jdoc:include type="component" />
+</body>
+</html>
